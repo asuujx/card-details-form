d1cfc920f6e6a83d2867deae5e300e127457b19b
assuy <43821373+assuy@users.noreply.github.com>
2023-10-17 12:01:29 +0200

    Add card information placeholder

--- app/components/CardDetails/CardFront.tsx
+++ app/components/CardDetails/CardFront.tsx
@@ -1,10 +1,17 @@
-
 function CardFront() {
   return (
-    <div className="w-3/4 pl-6 relative -top-20">
-      <img src="/images/bg-card-front.png" alt="card-front" />
+    <div className="w-80 h-44 ml-6 pl-6 relative -top-20 front-background rounded-md text-white">
+      <div className="flex mb-10 gap-4 place-items-center">
+        <div className="w-10 h-10 rounded-full bg-white"></div>
+        <div className="w-5 h-5 border-2 border-white rounded-full"></div>
+      </div>
+      <p className="text-2xl mb-3">0000 0000 0000 0000</p>
+      <div className="flex justify-between text-sm">
+        <p className="uppercase">Jane Appleseed</p>
+        <p>00/00</p>
+      </div>
     </div>
   );
 }
 
-export default CardFront
+export default CardFront;
